b912d31d1c1df842e3710ca0196aa5c82f38515b
Mikołaj Wargowski <mikoajw20@gmail.com>
2021-12-28 22:58:09 +0100

    🌈 Connect Swagger docs

---
+++ src/server/src/infrastructure/swagger/swagger.ts
@@ -0,0 +1,40 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import path from 'path';
+
+const modules = ['platform-access'];
+
+export const swaggerDocs = swaggerJSDoc({
+  swaggerDefinition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'Krater API',
+      version: '1.0.0',
+    },
+  },
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'x-auth-token',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+        adminAuth: [],
+      },
+    ],
+  },
+  apis: modules.map((module) =>
+    path.join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      `node_modules/@krater/${module}/dist/api/**/*.action.js`,
+    ),
+  ),
+});
